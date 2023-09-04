57057c3e40272f67494d2b117bc5c7a856287533
Hassaan Ahmed <hassaanahmedc@gmail.com>
2023-09-04 22:39:46 +0500

    Fixed navbar toggle manu on monile screens

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 const toggle_menu = document.getElementById("nav-menu");
 const menu_close = document.getElementById("nav-xmark");
-
 const toggle_display = document.querySelector(".nav-links");
 
 const open_toggle = () => {
-  if (window.getComputedStyle(toggle_display).display === "none") {
-    toggle_display.style.display = "block";
-  }
-  if (window.getComputedStyle(toggle_menu).cursor === "pointer") {
-    toggle_menu.style.document = "default";
-  } else {
-    toggle_menu.style.document = "pointer";
-  }
+  toggle_display.style.left = "-19px";
 };
 
 const close_toggle = () => {
-  if (window.getComputedStyle(toggle_display).display === "block") {
-    toggle_display.style.display = "none";
-  }
+  toggle_display.style.left = "-100%";
 };
 
 menu_close.addEventListener("click", close_toggle);
